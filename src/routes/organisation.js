684398f4eb9ebb50d65b89d0c3182b6c8ed81471
ZEDD2468 <victoradebayo360@gmail.com>
2024-07-10 02:35:40 +0100

    vercel

--- src/routes/organisation.js
+++ src/routes/organisation.js
@@ -6,7 +6,7 @@ const authenticateToken = require('../middleware/authMiddleware');
 // Organisation routes
 router.get('/:userId/organisations', authenticateToken, organisationController.getOrganisations);
 router.get('/api/organisations/:orgId', authenticateToken, organisationController.getOrganisationById);
-router.post('/', authenticateToken, organisationController.createOrganisation);
+router.post('/organisation', authenticateToken, organisationController.createOrganisation);
 router.post('/api/organisations/:orgId/users', authenticateToken, organisationController.addUserToOrganisation);
 
 
