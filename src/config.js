4dd07cf5b0088ca2272d383cf02575ba482cf008
ZEDD2468 <victoradebayo360@gmail.com>
2024-07-11 04:30:33 +0100

    victor

--- src/config.js
+++ src/config.js
@@ -1,41 +1,20 @@
 const { Sequelize } = require('sequelize');
-require('pg');
+
 
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: false,
-      rejectUnauthorized: false, // Set to true if you have a trusted certificate
-    },
-  },
-  logging: false,
+  logging: false, // Disable logging SQL queries to the console
   pool: {
-    max: 10,
-    min: 0,
-    idle: 10000,
-    acquire: 30000,
+    max: 10, // Maximum number of connections in the pool
+    min: 0, // Minimum number of connections in the pool
+    idle: 10000, // Maximum time, in milliseconds, that a connection can be idle before being released
+    acquire: 30000, // Maximum time, in milliseconds, that pool will try to get the connection before throwing error
   },
-  timezone: '+05:30',
+  timezone: '+05:30', // Example: Set the timezone to UTC+05:30
+  // Other options as needed
 });
 
 module.exports = sequelize;
-
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: 'postgres',
-//   protocol: 'postgres',
-//   logging: false, // Disable logging SQL queries to the console
-//   pool: {
-//     max: 10, // Maximum number of connections in the pool
-//     min: 0, // Minimum number of connections in the pool
-//     idle: 10000, // Maximum time, in milliseconds, that a connection can be idle before being released
-//     acquire: 30000, // Maximum time, in milliseconds, that pool will try to get the connection before throwing error
-//   },
-//   timezone: '+05:30', // Example: Set the timezone to UTC+05:30
-//   // Other options as needed
-// });
-
-// module.exports = sequelize;
